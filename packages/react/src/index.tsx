eb09a24bce4273eea99e81e18b2dad7b44097642
Danilo dos Santos <danilo.souza@brisabr.com.br>
2023-04-11 17:36:19 -0300

    configuração do stitches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,13 @@
-import { colors } from '@brisa-ui/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$gray500',
+  borderRadius: '$md',
+  padding: '$4',
+  height: '$10',
+})
 
 export function App() {
-  return <h1 style={{ color: colors.gray500 }}> Hello World</h1>
+  return <Button> Hello World</Button>
 }
